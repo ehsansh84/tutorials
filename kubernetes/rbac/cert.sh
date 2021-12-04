2f922435f5b1b4b5392ce6b65c8f78e9685918a7
ehsan <ehsan.shirzadi@gmail.com>
2021-12-04 14:34:09 +0330

    added: cert.sh to create kubeconfig

---
+++ kubernetes/rbac/cert.sh
@@ -0,0 +1,11 @@
+USER=ali
+NAMESPACE=anjo
+SERVER_IP=85.208.253.129
+openssl genrsa -out $USER.key 2048
+openssl req -new -key $USER.key -out $USER.csr -subj "/CN=$USER/O=$NAMESPACE"
+openssl x509 -req -in $USER.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out $USER.crt -days 365
+scp ubuntu@$SERVER_IP:/etc/kubernetes/pki/ca.{crt,key} .
+kubectl --kubeconfig $USER.kubeconfig config set-cluster kubernetes --server https://$SERVER_IP:6443 --certificate-authority=ca.crt --embed certs=true
+kubectl --kubeconfig $USER.kubeconfig config set-credentials $USER --client-certificate $USER.crt --client-key $USER.key --embed certs=true
+kubectl --kubeconfig $USER.kubeconfig config set-context $USER-kubernetes --cluster kubernetes --namespace $NAMESPACE --user $USER
+
